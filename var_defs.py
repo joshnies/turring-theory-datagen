f9db09565aedbb186ef0277259f1a49eff066bde
Josh Nies <50473759+joshnies@users.noreply.github.com>
2020-11-08 10:11:34 -0800

    Add AI_USER_TYPE var def pairs

--- var_defs.py
+++ var_defs.py
@@ -1,5 +1,5 @@
 import random
-from constants import AI_VAR_NAME, AI_VAL
+from constants import AI_VAR_NAME, AI_VAL, AI_USER_TYPE
 from cpp import CPP_PRIM_TYPES, gen_cpp_generic_type
 
 
@@ -29,6 +29,15 @@ def gen_var_defs(generic_count):
         (source, target) = gen_var_def_pair(t, has_default_value=True)
         item = {'source': source, 'target': target}
         data.append(item)
+
+    # Generate user type variable definition pair
+    (source, target) = gen_var_def_pair(AI_USER_TYPE, has_default_value=False)
+    item = {'source': source, 'target': target}
+    data.append(item)
+
+    (source, target) = gen_var_def_pair(AI_USER_TYPE, has_default_value=True)
+    item = {'source': source, 'target': target}
+    data.append(item)
 
     # Generate generic variable definition pairs
     for i in range(generic_count):
