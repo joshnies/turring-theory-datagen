c63ac2b2a147a8531f52cdae8d6b33c5d0fc04ec
Josh Nies <50473759+joshnies@users.noreply.github.com>
2020-11-15 06:39:23 -0800

    Add C++ bracket "includes" syntax

--- imports.py
+++ imports.py
@@ -4,6 +4,13 @@ from constants import AI_IMPORT
 def gen_path_imports():
     """Generate path imports."""
 
-    source = f'#include "{AI_IMPORT}"'
-    target = f'require("{AI_IMPORT}")'
-    return [{'source': source, 'target': target}]
+    return [
+        {
+            'source': f'#include "{AI_IMPORT}"',
+            'target': f'require("{AI_IMPORT}")'
+        },
+        {
+            'source': f'#include <{AI_IMPORT}>',
+            'target': f'require("{AI_IMPORT}")'
+        }
+    ]
