fc57445033d07b80bead2e61cad106f601d226ec
Josh Nies <50473759+joshnies@users.noreply.github.com>
2020-12-08 10:25:10 -0800

    Update class constructs

--- theory_data_gen/cpp17_nodejs14/class_constructs.py
+++ theory_data_gen/cpp17_nodejs14/class_constructs.py
@@ -1,13 +1,12 @@
 from tqdm import tqdm
-from theory_data_gen.common import gen_val_list
-from theory_data_gen.mask_tokens import AI_CLASS_NAME
+from theory_data_gen.common import gen_val_list, gen_mask_token
 
 
 def gen_class_construct_pair():
     """Generate a class construction pair."""
 
-    args = gen_val_list()
-    construct = f'new {AI_CLASS_NAME}({args});'
+    args, _ = gen_val_list(mask_index=1)
+    construct = f'new {gen_mask_token(0)}({args});'
     return construct
 
 
