e9b2c19a0bb5f5cbc3e4d876572969471dba3e1d
Josh Nies <50473759+joshnies@users.noreply.github.com>
2020-12-08 06:42:10 -0800

    Add import permutations

--- theory_data_gen/cpp17_nodejs14/imports.py
+++ theory_data_gen/cpp17_nodejs14/imports.py
@@ -1,17 +1,36 @@
 from theory_data_gen.common import gen_item
-from theory_data_gen.mask_tokens import AI_IMPORT
+from theory_data_gen.mask_tokens import MASK_TOKEN
 
 
-def gen_path_imports():
+def gen_path_imports(permutations: int):
     """Generate path imports."""
 
-    return [
-        gen_item(
-            f'#include "{AI_IMPORT}"',
-            f'require("{AI_IMPORT}")'
-        ),
-        gen_item(
-            f'#include <{AI_IMPORT}>',
-            f'require("{AI_IMPORT}")'
-        )
-    ]
+    # return [
+    #     gen_item(
+    #         f'#include "{AI_IMPORT}"',
+    #         f'require("{AI_IMPORT}")'
+    #     ),
+    #     gen_item(
+    #         f'#include <{AI_IMPORT}>',
+    #         f'require("{AI_IMPORT}")'
+    #     )
+    # ]
+
+    data = []
+
+    for i in range(permutations):
+        m = MASK_TOKEN.replace('n', str(i), 1)
+        items = [
+            gen_item(
+                f'#include "{m}"',
+                f'require("{m}");'
+            ),
+            gen_item(
+                f'#include <{m}>',
+                f'require("{m}");'
+            )
+        ]
+
+        data.extend(items)
+
+    return data
