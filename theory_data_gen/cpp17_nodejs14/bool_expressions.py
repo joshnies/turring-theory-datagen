3dd565369732c823d4aceb95c9a577f27bf97882
Josh Nies <50473759+joshnies@users.noreply.github.com>
2020-12-08 10:40:16 -0800

    Update bool expressions

--- theory_data_gen/cpp17_nodejs14/bool_expressions.py
+++ theory_data_gen/cpp17_nodejs14/bool_expressions.py
@@ -1,5 +1,4 @@
-from theory_data_gen.common import gen_item
-from theory_data_gen.mask_tokens import AI_EXTRACTION
+from theory_data_gen.common import gen_item, gen_mask_token
 
 
 def gen_bool_expressions():
@@ -14,11 +13,20 @@ def gen_bool_expressions():
 
     same_ops = ['>', '<', '>=', '<=', '&&', '||']
 
+    # TODO: Implement arithmetic expressions (reused)
+    # TODO: Implement entity chains (reused)
+    m_0 = gen_mask_token(0)
+    m_1 = gen_mask_token(1)
+
     for src_op in ops_map:
         data.append(
-            gen_item(f'{AI_EXTRACTION} {src_op} {AI_EXTRACTION}', f'{AI_EXTRACTION} {ops_map[src_op]} {AI_EXTRACTION}'))
+            gen_item(
+                f'{m_0} {src_op} {m_1}',
+                f'{m_0} {ops_map[src_op]} {m_1}'
+            )
+        )
 
     for op in same_ops:
-        data.append(gen_item(f'{AI_EXTRACTION} {op} {AI_EXTRACTION}', f'{AI_EXTRACTION} {op} {AI_EXTRACTION}'))
+        data.append(gen_item(f'{m_0} {op} {m_1}'))
 
     return data
