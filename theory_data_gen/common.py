b9ddf3375f520b849f4bd32accfb98add9baa72a
Josh Nies <50473759+joshnies@users.noreply.github.com>
2020-12-08 09:38:06 -0800

    Update func calls

--- theory_data_gen/common.py
+++ theory_data_gen/common.py
@@ -1,13 +1,20 @@
 import random
-import re
-from typing import List, Tuple
 
 from theory_data_gen.mask_tokens import AI_VAL, MASK_TOKEN
 from theory_data_gen.utils import join
 
 
-def gen_val_list():
+def gen_mask_token(i):
+    """Generate mask token with index."""
+
+    return MASK_TOKEN.replace('n', str(i), 1)
+
+
+def gen_val_list(mask_index: int):
     """Generate a value list."""
+
+    # TODO: Add support for arithmetic values
+    # TODO: Add support for entity chain values
 
     val_range = range(0, 11)
     val_count = random.choices(val_range, weights=(80, 70, 60, 40, 30, 20, 5, 4, 3, 2, 1), k=1)[0]
@@ -15,9 +22,9 @@ def gen_val_list():
 
     # Generate value list
     for i in range(val_count):
-        vals.append(AI_VAL)
+        vals.append(gen_mask_token(mask_index + i))
 
-    return join(vals, ', ')
+    return join(vals, ', '), mask_index + val_count - 1
 
 
 def gen_item(source, target=None):
@@ -33,32 +40,3 @@ def deduplicate(data):
     """Deduplicate data."""
 
     return [dict(t) for t in {tuple(d.items()) for d in data}]
-
-
-# def permutate(src_seq: str, tar_seq: str, permutations: int):
-#     """Generate mask permutations of the given masked sequences."""
-#
-#     data = []
-#
-#     # Get amount of mask tokens
-#     count = len(re.findall(MASK_TOKEN, src_seq))
-#     pm_list = list(range(count))
-#
-#     for i in range(permutations):
-#         pm_list = [x + i for x in pm_list]
-#         pm_src_seq = src_seq
-#         pm_tar_seq = tar_seq
-#
-#         for match_idx in range(len(re.findall(MASK_TOKEN, src_seq))):
-#             pm_src_seq = pm_src_seq.replace(MASK_TOKEN, MASK_TOKEN.replace('n', str(pm_list[match_idx])))
-#             pm_tar_seq = pm_tar_seq.replace(MASK_TOKEN, MASK_TOKEN.replace('n', str(pm_list[match_idx])))
-#
-#         data.append(gen_item(pm_src_seq, pm_tar_seq))
-#
-#     return data
-
-
-def gen_mask_token(i):
-    """Generate mask token with index."""
-
-    return MASK_TOKEN.replace('n', str(i), 1)
--- theory_data_gen/cpp17_nodejs14/func_calls.py
+++ theory_data_gen/cpp17_nodejs14/func_calls.py
@@ -1,24 +1,27 @@
 import random
 
 from tqdm import tqdm
-from theory_data_gen.common import gen_val_list
-from theory_data_gen.mask_tokens import AI_STMT_OBJ
+from theory_data_gen.common import gen_val_list, gen_mask_token
 from theory_data_gen.cpp17_nodejs14.generics import gen_provided_generics
 from theory_data_gen.utils import join, join_rand
 
 
-def gen_func_call_obj():
+def gen_func_call_obj(mask_index: int):
     """Generates an object or function call for a pair."""
 
+    # Generate mask tokens
+    m_name = gen_mask_token(mask_index)
+
+    # Generate data
     is_func_call = bool(random.getrandbits(1))
     func_call_markers = ['(', ')'] if is_func_call else ['', '']
-    args = gen_val_list() if is_func_call else ''
+    args, last_mask_index = gen_val_list(mask_index + 1) if is_func_call else ('', mask_index)
 
     # Generate generics
     generics = gen_provided_generics() if is_func_call else ''
 
-    obj = f'{AI_STMT_OBJ}{generics}{func_call_markers[0]}{args}{func_call_markers[1]}'
-    return obj
+    obj = f'{m_name}{generics}{func_call_markers[0]}{args}{func_call_markers[1]}'
+    return obj, last_mask_index
 
 
 def gen_func_call_pair():
@@ -29,8 +32,12 @@ def gen_func_call_pair():
     objs = []
 
     # Generate func args
+    next_mask_index = 0
+
     for i in range(length):
-        objs.append(gen_func_call_obj())
+        obj, last_mask_index = gen_func_call_obj(next_mask_index)
+        next_mask_index = last_mask_index + 1
+        objs.append(obj)
 
     source_objs = join_rand(objs, ['.', '->', '::'])
     target_objs = join(objs, '.')
