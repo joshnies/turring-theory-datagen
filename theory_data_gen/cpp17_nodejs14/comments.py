7b5a0f4184698eb75be810df039bf5d47c003133
Josh Nies <50473759+joshnies@users.noreply.github.com>
2020-12-08 10:36:28 -0800

    Update comments

--- theory_data_gen/cpp17_nodejs14/comments.py
+++ theory_data_gen/cpp17_nodejs14/comments.py
@@ -1,13 +1,14 @@
-from theory_data_gen.common import gen_item
-from theory_data_gen.mask_tokens import AI_COPY
+from theory_data_gen.common import gen_item, gen_mask_token
 
 
 def gen_comments():
     """Generate all comment data."""
 
+    m = gen_mask_token(0)
+
     data = [
-        gen_item(f'//{AI_COPY}'),
-        gen_item(f'/*{AI_COPY}*/')
+        gen_item(f'//{m}'),
+        gen_item(f'/*{m}*/')
     ]
 
     return data
