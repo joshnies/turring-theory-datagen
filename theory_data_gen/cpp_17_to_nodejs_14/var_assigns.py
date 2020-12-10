f334e561557c5fe099502decac1f89ce14251977
Josh Nies <50473759+joshnies@users.noreply.github.com>
2020-12-10 11:17:35 -0800

    Add random value for var assigns

--- theory_data_gen/cpp_17_to_nodejs_14/var_assigns.py
+++ theory_data_gen/cpp_17_to_nodejs_14/var_assigns.py
@@ -1,11 +1,48 @@
-from theory_data_gen.common import gen_mask_token
+import random
+from tqdm import tqdm
+
+from theory_data_gen.common import add_mask_indices, gen_item
+from theory_data_gen.constants import MASK_TOKEN
+from .arithmetic import gen_arithmetic
+from .entity_chains import gen_entity_chain_pair
 
 
-def gen_var_assigns():
+def __gen_var_assign():
+    """Generate a variable assignment."""
+
+    source_def_val = None
+    target_def_val = None
+
+    selection = random.choice(range(3))
+
+    if selection == 0:
+        # Mask token
+        def_val = MASK_TOKEN
+    elif selection == 1:
+        # Arithmetic/boolean expression
+        source_def_val, target_def_val = gen_arithmetic()
+    else:
+        # Entity chain
+        source_def_val, target_def_val = gen_entity_chain_pair(add_semicolon=False, should_add_mask_indices=False)
+
+    # Generate source/target
+    source = f'{MASK_TOKEN} = {source_def_val};'
+    target = f'{MASK_TOKEN} = {target_def_val};'
+
+    # Add mask indices
+    source, _ = add_mask_indices(source)
+    target, _ = add_mask_indices(target)
+
+    return source, target
+
+
+def gen_var_assigns(count: int):
     """Generate variable assignments."""
 
-    m_0 = gen_mask_token(0)
-    m_1 = gen_mask_token(1)
+    data = []
 
-    assign = f'{m_0} = {m_1};'
-    return [{'source': assign, 'target': assign}]
+    for _ in tqdm(range(count), desc='Generating variable assignments'):
+        source, target = __gen_var_assign()
+        data.append(gen_item(source, target))
+
+    return data
