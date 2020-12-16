8ff17919658a8e20b56bdbcec9c033ccbd0c5aae
Josh Nies <50473759+joshnies@users.noreply.github.com>
2020-12-16 06:54:50 -0800

    Remove brackets and semicolons from rogue entities

--- theory_data_gen/cpp_17_to_nodejs_14/rogue_entities.py
+++ theory_data_gen/cpp_17_to_nodejs_14/rogue_entities.py
@@ -7,13 +7,6 @@ def gen_rogue_entities():
     m_0 = gen_mask_token(0)
 
     return [
-        gen_item('{'),
-        gen_item('}'),
-        gen_item('};'),
-        gen_item('('),
-        gen_item(')'),
-        gen_item(');'),
-        gen_item(';'),
         # gen_item(m_0),
         # gen_item(f'{m_0},'),
         # gen_item(f'{m_0};'),
