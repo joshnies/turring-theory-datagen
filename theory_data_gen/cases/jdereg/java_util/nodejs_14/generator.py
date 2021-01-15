6ff9efb400bf9b56f8cf80119c5b1d5bf05f210b
Josh Nies <50473759+joshnies@users.noreply.github.com>
2021-01-15 10:36:39 -0800

    Implement case module system

---
+++ theory_data_gen/cases/jdereg/java_util/nodejs_14/generator.py
@@ -0,0 +1,14 @@
+from theory_data_gen.generator import Generator
+
+
+class JderegJavaUtilGenerator(Generator):
+    """
+    Case generator for "jdereg/java-util":
+    https://github.com/jdereg/java-util
+    """
+
+    @staticmethod
+    def generate(args, write):
+        print('\nGenerating case data for "jdereg/java-util"')
+
+        # TODO: Add data
