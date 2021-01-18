628c15a394cb7d635c12566789f689f794503a01
Josh Nies <50473759+joshnies@users.noreply.github.com>
2021-01-18 08:12:30 -0800

    Make entity count args optional

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 import argparse
-import csv
-import time
 
 from theory_data_gen.lvp import LVP
-from theory_data_gen.output import create_output_file, deduplicate_lines, CSV_COLUMNS
+from theory_data_gen.output import create_output_file, deduplicate_lines
 from theory_data_gen.base.cpp_17_to_nodejs_14.generator import Cpp17ToNodeJS14Generator
 from theory_data_gen.base.java_14_to_nodejs_14.generator import Java14ToNodeJS14Generator
 from theory_data_gen.base.java_14_to_python_3.generator import Java14ToPython3Generator
@@ -16,22 +14,22 @@ parser.add_argument('-c', '--case', help='Case module used for additional data g
 parser.add_argument('-o', '--out', help='Output file path', required=True)
 parser.add_argument('--minimal', help='Whether to only generate minimal required data for the LVP. Intended for use ' +
                                       'with a case module.', action='store_true')
-parser.add_argument('--vars', help='Number of variables', type=int, required=True)
-parser.add_argument('--arr-vars', help='Number of array variables', type=int, required=True)
+parser.add_argument('--vars', help='Number of variables', type=int, default=0)
+parser.add_argument('--arr-vars', help='Number of array variables', type=int, default=0)
 parser.add_argument('--arr-var-defs', help='Number of array variable definitions (with no default value)', type=int,
-                    required=True)
-parser.add_argument('--functions', help='Number of functions', type=int, required=True)
-parser.add_argument('--entity-chains', help='Number of entity chains', type=int, required=True)
-parser.add_argument('--classes', help='Number of classes', type=int, required=True)
-parser.add_argument('--class-constructs', help='Number of class construction statements', type=int, required=True)
-parser.add_argument('--conditionals', help='Number of conditional structures', type=int, required=True)
-parser.add_argument('--switches', help='Number of switch structures', type=int, required=True)
-parser.add_argument('--switch-cases', help='Number of switch cases', type=int, required=True)
-parser.add_argument('--loops', help='Number of loops (excluding "for" loops)', type=int, required=True)
-parser.add_argument('--for-loop-inputs', help='Number of rogue "for" loop inputs', type=int, required=True)
-parser.add_argument('--arithmetic', help='Number of arithmetic expressions', type=int, required=True)
-parser.add_argument('--returns', help='Number of return statements', type=int, required=True)
-parser.add_argument('--cout', help='Number of console output statements', type=int)
+                    default=0)
+parser.add_argument('--functions', help='Number of functions', type=int, default=0)
+parser.add_argument('--entity-chains', help='Number of entity chains', type=int, default=0)
+parser.add_argument('--classes', help='Number of classes', type=int, default=0)
+parser.add_argument('--class-constructs', help='Number of class construction statements', type=int, default=0)
+parser.add_argument('--conditionals', help='Number of conditional structures', type=int, default=0)
+parser.add_argument('--switches', help='Number of switch structures', type=int, default=0)
+parser.add_argument('--switch-cases', help='Number of switch cases', type=int, default=0)
+parser.add_argument('--loops', help='Number of loops (excluding "for" loops)', type=int, default=0)
+parser.add_argument('--for-loop-inputs', help='Number of rogue "for" loop inputs', type=int, default=0)
+parser.add_argument('--arithmetic', help='Number of arithmetic expressions', type=int, default=0)
+parser.add_argument('--returns', help='Number of return statements', type=int, default=0)
+parser.add_argument('--cout', help='Number of console output statements', type=int, default=0)
 args = parser.parse_args()
 
 # Get LVP from args
