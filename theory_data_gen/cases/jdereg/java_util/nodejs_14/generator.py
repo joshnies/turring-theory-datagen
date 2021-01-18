6bafec475d2c18e9b66ba826afe2bbc3389ece31
Josh Nies <50473759+joshnies@users.noreply.github.com>
2021-01-18 06:43:00 -0800

    Update jdereg/java-util case with member prefixes

--- theory_data_gen/cases/jdereg/java_util/nodejs_14/generator.py
+++ theory_data_gen/cases/jdereg/java_util/nodejs_14/generator.py
@@ -1,4 +1,5 @@
 from theory_data_gen.common import gen_item, gen_mask_token
+from theory_data_gen.constants import MEMBER_TOKEN
 from theory_data_gen.generator import Generator
 
 
@@ -18,7 +19,7 @@ class JderegJavaUtilGenerator(Generator):
                 f'class {gen_mask_token(0)} implements {gen_mask_token(3)} {{'
             ),
             gen_item(
-                f'private final {gen_mask_token(0)}<{gen_mask_token(1)}, {gen_mask_token(2)}> {gen_mask_token(3)};'
+                f'{MEMBER_TOKEN} private final {gen_mask_token(0)}<{gen_mask_token(1)}, {gen_mask_token(2)}> {gen_mask_token(3)};'
                 f'private {gen_mask_token(3)};'
             ),
             gen_item(
@@ -44,23 +45,23 @@ class JderegJavaUtilGenerator(Generator):
                 f'{gen_mask_token(0)}.{gen_mask_token(1)}({gen_mask_token(3)});'
             ),
             gen_item(
-                f'public {gen_mask_token(0)} {gen_mask_token(1)}({gen_mask_token(2)} {gen_mask_token(3)}, {gen_mask_token(4)} {gen_mask_token(5)}) {{',
-                f'const {gen_mask_token(1)} = ({gen_mask_token(3)}, {gen_mask_token(5)}) => {{'
+                f'{MEMBER_TOKEN} public {gen_mask_token(0)} {gen_mask_token(1)}({gen_mask_token(2)} {gen_mask_token(3)}, {gen_mask_token(4)} {gen_mask_token(5)}) {{',
+                f'{gen_mask_token(1)}({gen_mask_token(3)}, {gen_mask_token(5)}) {{'
             ),
             gen_item(
                 f'return {gen_mask_token(0)}.{gen_mask_token(1)}({gen_mask_token(2)}, {gen_mask_token(3)});'
             ),
             gen_item(
-                f'public boolean {gen_mask_token(0)}({gen_mask_token(1)} {gen_mask_token(2)}) {{',
-                f'const {gen_mask_token(0)} => ({gen_mask_token(2)}) {{'
+                f'{MEMBER_TOKEN} public boolean {gen_mask_token(0)}({gen_mask_token(1)} {gen_mask_token(2)}) {{',
+                f'{gen_mask_token(0)}({gen_mask_token(2)}) {{'
             ),
             gen_item(
                 f'boolean {gen_mask_token(0)} = {gen_mask_token(1)}.{gen_mask_token(2)}({gen_mask_token(3)});'
                 f'let {gen_mask_token(0)} = {gen_mask_token(1)}.{gen_mask_token(2)}({gen_mask_token(3)});'
             ),
             gen_item(
-                f'public void {gen_mask_token(0)}(Map<? extends {gen_mask_token(1)}, ? extends {gen_mask_token(2)}> {gen_mask_token(3)}) {{',
-                f'const {gen_mask_token(0)} => ({gen_mask_token(3)}) => {{'
+                f'{MEMBER_TOKEN} public void {gen_mask_token(0)}(Map<? extends {gen_mask_token(1)}, ? extends {gen_mask_token(2)}> {gen_mask_token(3)}) {{',
+                f'{gen_mask_token(0)}({gen_mask_token(3)}) {{'
             ),
             gen_item(
                 f'{gen_mask_token(0)}.{gen_mask_token(1)}({gen_mask_token(2)});'
@@ -70,34 +71,34 @@ class JderegJavaUtilGenerator(Generator):
                 f'return {gen_mask_token(0)} instanceof Object && {gen_mask_token(1)}.{gen_mask_token(2)}({gen_mask_token(3)});'
             ),
             gen_item(
-                f'public Collection<{gen_mask_token(0)}> {gen_mask_token(1)}() {{',
-                f'const {gen_mask_token(1)} = () => {{'
+                f'{MEMBER_TOKEN} public Collection<{gen_mask_token(0)}> {gen_mask_token(1)}() {{',
+                f'{gen_mask_token(1)}() {{'
             ),
             gen_item(
-                f'public Set<{gen_mask_token(0)}> {gen_mask_token(1)}() {{',
-                f'const {gen_mask_token(1)} = () => {{'
+                f'{MEMBER_TOKEN} public Set<{gen_mask_token(0)}> {gen_mask_token(1)}() {{',
+                f'{gen_mask_token(1)}() {{'
             ),
             gen_item(
-                f'public Set<Entry<{gen_mask_token(0)}, {gen_mask_token(1)}>> {gen_mask_token(2)}() {{',
-                f'const {gen_mask_token(2)} = () => {{'
+                f'{MEMBER_TOKEN} public Set<Entry<{gen_mask_token(0)}, {gen_mask_token(1)}>> {gen_mask_token(2)}() {{',
+                f'{gen_mask_token(2)}() {{'
             ),
             gen_item(
                 f'{gen_mask_token(0)}.{gen_mask_token(1)}().{gen_mask_token(2)}({gen_mask_token(3)});'
             ),
             gen_item(
-                f'public void {gen_mask_token(0)}(Collection<{gen_mask_token(1)}> {gen_mask_token(2)}) {{',
-                f'const {gen_mask_token(0)} = ({gen_mask_token(2)}) => {{'
+                f'{MEMBER_TOKEN} public void {gen_mask_token(0)}(Collection<{gen_mask_token(1)}> {gen_mask_token(2)}) {{',
+                f'{gen_mask_token(0)}({gen_mask_token(2)}) {{'
             ),
             gen_item(
-                f'public void {gen_mask_token(0)}({gen_mask_token(1)}<{gen_mask_token(2)}, {gen_mask_token(3)}> {gen_mask_token(4)}) {{',
-                f'const {gen_mask_token(0)} = ({gen_mask_token(4)}) => {{'
+                f'{MEMBER_TOKEN} public void {gen_mask_token(0)}({gen_mask_token(1)}<{gen_mask_token(2)}, {gen_mask_token(3)}> {gen_mask_token(4)}) {{',
+                f'{gen_mask_token(0)}({gen_mask_token(4)}) {{'
             ),
             gen_item(
                 f'{gen_mask_token(0)}.{gen_mask_token(1)}({gen_mask_token(2)}.{gen_mask_token(3)});'
             ),
             gen_item(
-                f'public Map {gen_mask_token(0)}() {{',
-                f'const {gen_mask_token(0)} => () => {{'
+                f'{MEMBER_TOKEN} public Map {gen_mask_token(0)}() {{',
+                f'{gen_mask_token(0)}() {{'
             )
         ]
 
