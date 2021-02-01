45c223c185a2913d36234eee88d4b6b0c416b882
BobNies <elsereturn@gmail.com>
2021-01-31 22:58:58 -0800

    var type in progress

--- theory_data_gen/base/ibm_cobol_to_cs_7/vars.py
+++ theory_data_gen/base/ibm_cobol_to_cs_7/vars.py
@@ -1,25 +1,56 @@
 import random
 
+from theory_data_gen.common import gen_item, add_mask_indices, gen_mask_token
 from theory_data_gen.constants import MASK_TOKEN
+
 
 def __gen_var_type():
     """Generate variable type."""
 
-    choice = random.choice(range(0, 3))
+    choice = random.choice(range(0, 5))
+
+    # we really don't care about the range size unless it's 18 - then it's a 'long'
+    int_arg_range = [3, 4, 9, 18]
+    int_arg_count = random.choices(int_arg_range, weights=(10, 40, 40, 10), k=1)[0]
 
     if choice == 0:
         # Generate alphanumeric type
-        return f'X({<RANDOM_NUM>})'
+        str_arg_range = range(1, 100)
+        return f'X({str_arg_range})'
     elif choice == 1:
-        # Generate ....
+        # Generate numeric
+        return f'9({int_arg_count})'
+    elif choice == 2:
+        # Generate char
+        return f'N'
+    elif choice == 3:
+        # Generate alphabetic
+        return f'A({int_arg_count})'
+    elif choice == 4:
+        # Generate float
+        rand_decimal_places = range(1, 9)
+        return f' S9({int_arg_count})V9{rand_decimal_places}'
     else:
-        # Generate ...
+        # Generate signed numeric
+        return f'S9({int_arg_count})'
+
 
 def __gen_var_items():
     """Generate variable items."""
 
+    # Generate source/target
     # 01 VarName PIC <type>.
-    src = f'{MASK_TOKEN} {MASK_TOKEN} PIC {__gen_var_type()}.'
+    source = f'{MASK_TOKEN} {MASK_TOKEN} PIC {__gen_var_type()}.'
+    # TODO - how do we know if private/public?
+    # TODO - what index used with gen_mask_token?
+    # TODO - how do we handle default vals
+    target = f'private {gen_mask_token(0)} todo'
+
+    item = gen_item(source, target)
+    item, _ = add_mask_indices(item)
+
+    return [item]
+
 
 def gen_vars(write, count):
     """Generate variables."""
---
+++ theory_data_gen/common/ibm_cobol.py
@@ -0,0 +1,14 @@
+COBOL_PIC_TYPES = [
+    'A',  # alphabetic or space   -string
+    'B',  # used with Display
+    'E',  # floating point marker
+    'G',  # A DBCS character position
+    'N',  # char
+    'P',  # assumed decimal-float
+    'S9', # signed numeric-int
+    'V',  # decimal       -float
+    'Z',  # rand_decimal_places
+    'X',  # alphanumeric  -string
+    '9',  # numeric       -int
+    '0',  # numeral zero inserted only
+]
