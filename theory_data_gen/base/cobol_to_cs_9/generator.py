55a03d0c46269fb623bdfb68800ae3c10446bec5
Josh Nies <50473759+joshnies@users.noreply.github.com>
2021-02-04 15:27:36 -0800

    Rename cobol to C# LVP to use C# 9

--- theory_data_gen/base/cobol_to_cs_9/generator.py
+++ theory_data_gen/base/cobol_to_cs_9/generator.py
@@ -5,18 +5,19 @@ from .stdout import gen_stdout
 from .vars import gen_vars
 
 
-class CobolToCS7Generator(Generator):
-    """Data generator for COBOL to C# 7."""
+class CobolToCS9Generator(Generator):
+    """Data generator for COBOL to C# 9."""
 
     @staticmethod
     def generate(args, write):
-        print('\nGenerating dataset for COBOL --> C# 7')
+        print('\nGenerating dataset for COBOL --> C# 9')
 
         gen_vars(write)
         gen_arithmetic(write, args.arithmetic)
         gen_conditionals(write, args.conditionals)
         gen_stdout(write)
 
-        # TODO: Implement loops
+        # TODO: Implement COBOL "PERFORM" statements to C# loops
         # TODO: Implement built-in function calls
-        # TODO: Implement COBOL subroutines (freeform function calls)
+        # TODO: Implement COBOL "EVALUATE" statements to C# switch statements
+        # TODO: Implement COBOL subroutines to C# function calls
