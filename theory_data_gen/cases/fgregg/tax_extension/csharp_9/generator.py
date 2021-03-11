f0dc38e4c5d79cc09296e64382eae24e8090fe28
Josh Nies <50473759+joshnies@users.noreply.github.com>
2021-03-11 15:26:47 -0800

    Fix fgregg/tax_extension case item

--- theory_data_gen/cases/fgregg/tax_extension/csharp_9/generator.py
+++ theory_data_gen/cases/fgregg/tax_extension/csharp_9/generator.py
@@ -18,7 +18,7 @@ class FGREGGTaxExtensionToCSharp9Generator(Generator):
         items = [
             gen_item(
                 f'{gen_mask_token(0)} {gen_mask_token(1)} OCCURS {gen_mask_token(2)} TIMES PIC XX',
-                f'var {gen_mask_token(1)} = new COBOLVar(new string("", 2), size: 2, occurs: {gen_mask_token(2)});'
+                f"var {gen_mask_token(1)} = new COBOLVar(new string(' ', 2), size: 2, occurs: {gen_mask_token(2)});"
             ),
         ]
 
