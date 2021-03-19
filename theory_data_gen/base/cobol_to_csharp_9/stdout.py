cacbeb080e31127eb797e039d6da20c4e91eeda3
Josh Nies <50473759+joshnies@users.noreply.github.com>
2021-03-19 13:10:38 -0700

    Add support for COBOL "DISPLAY" statements with single quotes

--- theory_data_gen/base/cobol_to_csharp_9/stdout.py
+++ theory_data_gen/base/cobol_to_csharp_9/stdout.py
@@ -17,6 +17,7 @@ def __gen_write_line(val_list: List[str]):
 
     # Generate targets
     tar_vals = ' + '.join(val_list)
+    tar_vals = tar_vals.replace("'", '"')
     tar = f'Console.WriteLine({tar_vals});'
     tar, _ = add_mask_indices(tar)
 
@@ -34,6 +35,7 @@ def __gen_write(val_list: List[str]):
 
     # Generate targets
     tar_vals = ' + '.join(val_list)
+    tar_vals = tar_vals.replace("'", '"')
     tar = f'Console.Write({tar_vals});'
     tar, _ = add_mask_indices(tar)
 
@@ -49,28 +51,29 @@ def gen_stdout(write):
     """
 
     with yaspin(text='Generating stdout...', color='magenta'):
-        for str_count in range(0, 5):
-            for var_count in range(0, 5):
-                # Skip case where both counts are 0
-                if str_count == 0 and var_count == 0:
-                    continue
+        for quote in ['"', "'"]:
+            for str_count in range(0, 5):
+                for var_count in range(0, 5):
+                    # Skip case where both counts are 0
+                    if str_count == 0 and var_count == 0:
+                        continue
 
-                # Generate base value list
-                vals = [f'"{MASK_TOKEN}"'] * str_count
-                vals.extend([MASK_TOKEN] * var_count)
+                    # Generate base value list
+                    vals = [MASK_TOKEN] * var_count
+                    vals.extend([f'{quote}{MASK_TOKEN}{quote}'] * str_count)
 
-                # Generate permutations
-                if str_count == 0 or var_count == 0:
-                    val_lists = [vals]
-                else:
-                    val_lists = list(permutations(vals))
-                    val_lists = list(map(lambda l: list(l), val_lists))
+                    # Generate permutations
+                    if str_count == 0 or var_count == 0:
+                        val_lists = [vals]
+                    else:
+                        val_lists = list(permutations(vals))
+                        val_lists = list(map(lambda l: list(l), val_lists))
 
-                for v in val_lists:
-                    # Generate line write
-                    item = __gen_write_line(v)
-                    write(item)
+                    for v in val_lists:
+                        # Generate line write
+                        item = __gen_write_line(v)
+                        write(item)
 
-                    # Generate same-line write
-                    item = __gen_write(v)
-                    write(item)
+                        # Generate same-line write
+                        item = __gen_write(v)
+                        write(item)
